acb26c166a6bb2279df64d6bcaa4dfd165e81d1e
Shaw Lu <shawlu95@gmail.com>
2022-05-17 23:45:52 -0700

    Validate signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  return res.send('signup');
+const validators = [
+  body('email').isEmail().withMessage('Invalid email'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Length must be 4~20'),
+];
+
+router.post('/api/users/signup', validators, (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array());
+  }
+
+  const { email, password } = req.body;
+  console.log('Creating user', email);
+  return res.send({ email, password });
 });
 
 export { router as signupRouter };
