9653a7d37b8227ec23a16b2e30f263b9cd146bec
Shaw Lu <shawlu95@gmail.com>
2022-05-19 23:06:25 -0700

    Package build method as static method

--- auth/src/model/user.ts
+++ auth/src/model/user.ts
@@ -6,6 +6,11 @@ interface UserAttrs {
   password: string;
 }
 
+// Describe property (static method) of user model
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -17,10 +22,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+export { User };
