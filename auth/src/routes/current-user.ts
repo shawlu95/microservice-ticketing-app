a2162d0aff1db4b42108deea01f762c575c7a7f8
Shaw Lu <shawlu95@gmail.com>
2022-05-21 10:50:29 -0700

    Refactor current user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,13 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 
+import { currentUser } from '../middlewares/current-user';
+
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  // Equivalent to (!req.session || !req.session.jwt)
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.jwt!);
-    return res.json(payload);
-  } catch (err) {
-    console.log('Invalid JWT', err);
-    return res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  // if undefined, send back null
+  return res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
