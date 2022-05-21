46828b9fbb3c8b558b3b960d555b12a2d424f7f7
Shaw Lu <shawlu95@gmail.com>
2022-05-21 10:18:32 -0700

    Implement get current user route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  return res.send('hello');
+  // Equivalent to (!req.session || !req.session.jwt)
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.jwt!);
+    return res.json(payload);
+  } catch (err) {
+    console.log('Invalid JWT', err);
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
