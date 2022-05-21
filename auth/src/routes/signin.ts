f378b49ec1bd05c96fc9ef4dfd135c2653c47c72
Shaw Lu <shawlu95@gmail.com>
2022-05-21 10:05:43 -0700

    Complete sign-in route handler

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../model/user';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import { StatusCodes } from 'http-status-codes';
 
 const router = express.Router();
 
@@ -14,7 +20,30 @@ router.post(
   validators,
   validateRequest,
   async (req: Request, res: Response) => {
-    return res.send('signin');
+    const { email, password } = req.body;
+    const existing = await User.findOne({ email });
+    if (!existing) {
+      // share as little info as possible when login
+      throw new BadRequestError('Invalid credential');
+    }
+
+    const match = await Password.compare(existing.password, password);
+    if (!match) {
+      throw new BadRequestError('Invalid credential');
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existing.id,
+        email: existing.email,
+      },
+      process.env.jwt! // suppress typescript warning
+    );
+
+    // attach jwt to cookie (must be https protocol)
+    req.session = { jwt: userJwt };
+
+    return res.status(StatusCodes.OK).json(existing);
   }
 );
 
