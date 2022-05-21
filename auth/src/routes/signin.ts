d7bbac0e4f9457c16f5add2641f19dbcc21a1b54
Shaw Lu <shawlu95@gmail.com>
2022-05-21 09:48:43 -0700

    Implement validation checks for sign-in

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationErorr } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  return res.send('signin');
-});
+const validators = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('Please enter a password'),
+];
+
+router.post(
+  '/api/users/signin',
+  validators,
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationErorr(errors.array());
+    }
+
+    return res.send('signin');
+  }
+);
 
 export { router as signinRouter };
