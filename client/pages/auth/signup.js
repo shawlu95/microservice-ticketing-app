6027ade8315de286703198d579eded890c29b4b6
Shaw Lu <shawlu95@gmail.com>
2022-05-21 17:52:37 -0700

    Display error on browser

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -5,15 +5,21 @@ import axios from "axios";
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
   const onSubmit = async (event) => {
     // Do not reload page
     event.preventDefault();
 
-    // Create a new user, should see cookie in browser
-    const res = await axios.post('/api/users/signup', {
-      email, password
-    });
-    console.log(res.data);
+    try {
+      // Create a new user, should see cookie in browser
+      const res = await axios.post('/api/users/signup', {
+        email, password
+      });
+      console.log(res.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return ( 
@@ -34,6 +40,12 @@ const Signup = () => {
           type="password" 
           className="form-control" />
       </div>
+      {errors.length > 0 && <div className="alert alert-danger">
+        <h4>Ooops...</h4>
+        <ul className="my-0">
+          {errors.map(err => <li key={err.message}>{err.message}</li>)}
+        </ul>
+      </div>}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   )
