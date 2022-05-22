0fb8fd107f2e933d6ad92fdc87196d62127a1dcc
Shaw Lu <shawlu95@gmail.com>
2022-05-22 10:36:54 -0700

    Global header and initial props
    - Need to manually call component's initial props
    - Drill down props into component
    - Global header is placed in _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,34 @@
 // global css files must be imported here
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-// NextJS will wrap component in custom wrap component
-export default ({ Component, pageProps}) => {
-  return <Component {...pageProps} />
+/**
+ * @dev NextJS will wrap component in custom wrap component
+ * @dev Common props are returned from getInitialProps and 
+ * drilled down to every component
+ */
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  )
 };
+
+AppComponent.getInitialProps = async ({ Component, ctx: { req } }) => {
+  const axios = buildClient({ req });
+  const { data } = await axios.get('/api/users/currentuser');
+
+  let pageProps;
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps({ req });
+  }
+  
+  return {
+    ...data,
+    pageProps
+  };
+};
+
+export default AppComponent;
