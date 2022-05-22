58626000806b57176639f1f72cf5abc3ece25ac9
Shaw Lu <shawlu95@gmail.com>
2022-05-22 14:08:48 -0700

    Refactor Mongo URI as env variable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,12 @@ const start = async () => {
     throw new Error('JWT key is undefined');
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI is undefined');
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('connected to mongodb');
   } catch (err) {
     console.error(err);
