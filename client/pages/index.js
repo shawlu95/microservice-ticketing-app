38722957fc298d13525380bc5cf1c040a5c3e15c
Shaw Lu <shawlu95@gmail.com>
2022-05-21 18:43:20 -0700

    Intro to server-side render

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,17 @@
 import Banana from "./banana";
-const App = () => {
+const App = ({ color }) => {
+  console.log('from component: ', color);
   return (<>
   <h1>Landing page</h1>
   <Banana />
   </>);
+};
+
+// Executed on server, components are rendered only once
+// Fetch some data for initial rendering of component
+App.getInitialProps = () => {
+  console.log('Reder initial props')
+  return { color: 'red' }; // props for App component
 };
 
 export default App;
