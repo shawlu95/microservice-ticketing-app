56dab2ae7907b703c054b9a231471ecdae9dcf08
Shaw Lu <shawlu95@gmail.com>
2022-05-24 02:48:38 -0700

    Create Listener abstract class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
@@ -33,3 +33,51 @@ client.on('connect', () => {
 
 process.on('SIGINT', () => client.close());
 process.on('SIGTERM', () => client.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+
+  private client: Stan;
+
+  // subclass can define it
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDeliverAllAvailable()
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const sub = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    sub.on('message', (msg: Message) => {
+      const seq = msg.getSequence() as number;
+      console.log(`${this.subject}:${this.queueGroupName} received ${seq}`);
+
+      const parsed = this.parseMessage(msg);
+      this.onMessage(parsed, msg);
+      msg.ack();
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+}
