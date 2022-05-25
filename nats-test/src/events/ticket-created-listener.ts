ff0c70e1c655e695d38b360124a4931feee8a0e8
Shaw Lu <shawlu95@gmail.com>
2022-05-24 21:02:19 -0700

    Typescript generic & type checking

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,18 @@
 import { Listener } from './base-listener';
 import { Message } from 'node-nats-streaming';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  // declare type to enforce "final", can't be reassigned
+  // subject: Subjects.TicketCreated = Subjects.TicketCreated;
+
+  // can also use readonly
+  readonly subject = Subjects.TicketCreated;
+
   queueGroupName = 'payment-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('ticket:created', data);
     msg.ack();
   }
