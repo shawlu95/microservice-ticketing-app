5bb6ea73679cd62c5ef845748fc39c11f0de1ec1
Shaw Lu <shawlu95@gmail.com>
2022-06-02 08:46:42 -0700

    Implement index (get all orders) route and test
    - Retrieve all orders belonging to signed-in user
    - Use populate to retrieve associated ticket

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,49 @@
+import { StatusCodes } from 'http-status-codes';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+  return ticket;
+};
+
+it('fetches orders for a particular user', async () => {
+  const ticket1 = await buildTicket();
+  const ticket2 = await buildTicket();
+  const ticket3 = await buildTicket();
+
+  const user1 = global.signin();
+  const user2 = global.signin();
+
+  // create one order as user1
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({ ticketId: ticket1.id });
+
+  // create two orders as user2
+  const { body: order1 } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user2)
+    .send({ ticketId: ticket2.id });
+  const { body: order2 } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user2)
+    .send({ ticketId: ticket3.id });
+
+  const res = await request(app)
+    .get('/api/orders')
+    .set('Cookie', user2)
+    .expect(StatusCodes.OK);
+  expect(res.body.length).toEqual(2);
+  expect(res.body[0].id).toEqual(order1.id);
+  expect(res.body[1].id).toEqual(order2.id);
+  expect(res.body[0].ticket.id).toEqual(ticket2.id);
+  expect(res.body[1].ticket.id).toEqual(ticket3.id);
+});
