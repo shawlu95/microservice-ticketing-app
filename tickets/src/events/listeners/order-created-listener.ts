184989603af750593b225760e6bbd06ef34dcfc2
Shaw Lu <shawlu95@gmail.com>
2022-06-03 23:16:53 -0700

    Must emit ticket-updated-event when orderId is set/unset
    - otherwise the orders service would become out-of-sync
    - ticket-updated-publisher doesn't support orderId property
    - order-created-listener is a subclass of listener which already has a private nats client
    - we can mark the client as protected so the child classes can access
    - this is better than passing in another ref of nats client, also makes testing easier

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -7,6 +7,7 @@ import {
 } from '@shawtickets/common';
 import { Ticket } from '../../models/ticket';
 import { queueGroupName } from './queue-group-name';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -14,17 +15,28 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   /**
    * @notice We use an orderId field to mark the ticket as locked.
-   * The field can be used to retrieve user creating the order */
+   * The field can be used to retrieve user creating the order.
+   * The listener also pubishes its own ticket-updated-event
+   * when orderId is set (mark the ticker as locked) */
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    const { id, ticket } = data;
-    const fetchedTicket = await Ticket.findById(ticket.id);
+    const { id: orderId } = data;
+    const ticket = await Ticket.findById(data.ticket.id);
 
-    if (!fetchedTicket) {
+    if (!ticket) {
       throw new NotFoundError();
     }
 
-    fetchedTicket.set({ orderId: id });
-    await fetchedTicket.save();
+    ticket.set({ orderId });
+    await ticket.save();
+
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: orderId,
+    });
 
     msg.ack();
   }
