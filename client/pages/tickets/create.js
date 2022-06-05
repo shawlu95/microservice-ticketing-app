d307954fda3128f4f904aa8d79f476e6475e989b
Shaw Lu <shawlu95@gmail.com>
2022-06-05 12:26:31 -0700

    Client: create ticket

---
+++ client/pages/tickets/create.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const CreateTicket = () => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/tickets',
+    method: 'post',
+    body: {
+      title,
+      price,
+    },
+    onSuccess: () => Router.push('/'),
+  });
+
+  // onBlur is triggered when user clicks away from first responder
+  // round dollar to two decimal
+  const onBlur = () => {
+    const value = parseFloat(price);
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value.toFixed(2));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    doRequest();
+  };
+
+  return (
+    <div ca>
+      <h1>Create Ticket</h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label>Title</label>
+          <input
+            className='form-control'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className='form-group'>
+          <label>Price</label>
+          <input
+            className='form-control'
+            onBlur={onBlur}
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        {errors}
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateTicket;
