121544bf6d4072e0e4cb1c26b18af60d25569522
Shaw Lu <shawlu95@gmail.com>
2022-06-05 13:03:30 -0700

    Client: router push to wildcard route

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const TicketDetail = ({ ticket }) => {
@@ -5,7 +6,8 @@ const TicketDetail = ({ ticket }) => {
     url: '/api/orders',
     method: 'post',
     body: { ticketId: ticket.id },
-    onSuccess: (order) => console.log(order),
+    onSuccess: (order) =>
+      Router.push('/orders/[orderId]', `/orders/${order.id}`),
   });
   return (
     <div>
