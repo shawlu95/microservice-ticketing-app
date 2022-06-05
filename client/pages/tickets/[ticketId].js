827baeada83c7ca7f69d568ce1cf2e74ed47cfa6
Shaw Lu <shawlu95@gmail.com>
2022-06-05 12:53:01 -0700

    Client: getInitialProps in ticket detail page and create order

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,27 @@
-const TicketDetail = () => {
-  return <div>Detail</div>;
+import useRequest from '../../hooks/use-request';
+
+const TicketDetail = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: { ticketId: ticket.id },
+    onSuccess: (order) => console.log(order),
+  });
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={doRequest} className='btn btn-primary'>
+        Purchase
+      </button>
+    </div>
+  );
 };
 
+TicketDetail.getInitialProps = async (context, axios) => {
+  const { ticketId } = context.query;
+  const { data } = await axios.get(`/api/tickets/${ticketId}`);
+  return { ticket: data };
+};
 export default TicketDetail;
