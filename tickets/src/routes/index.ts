a4b57eb1433ef0fed7e6f16cdf6bb97828441f87
Shaw Lu <shawlu95@gmail.com>
2022-06-05 14:22:47 -0700

    Tickets service: only list unreserved tickets

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -6,7 +6,7 @@ import { Ticket } from '../models/ticket';
 const router = express.Router();
 
 router.get('/api/tickets', async (req: Request, res: Response) => {
-  const tickets = await Ticket.find({});
+  const tickets = await Ticket.find({ orderId: undefined });
   return res.status(StatusCodes.OK).send(tickets);
 });
 
