20115e59a4b3a86a7e0413590f23b3db3d057b68
Shaw Lu <shawlu95@gmail.com>
2022-06-05 23:32:02 -0700

    Configure live domain

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://microservice-ticketing-app.xyz/',
       headers: req.headers,
     });
   } else {
