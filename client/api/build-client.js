7413d01436631c5aa74ece6bc1f7fa0c98dddb2b
Shaw Lu <shawlu95@gmail.com>
2022-06-17 09:55:17 -0700

    Set local baseURL in client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,8 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on server
     return axios.create({
-      baseURL: 'http://microservice-ticketing-app.xyz/',
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers,
     });
   } else {
